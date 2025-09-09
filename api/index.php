b272ac647a50055e9d4aeaefde61e67465c2b5d4
Vincent-Jung <117348000+Vincent-Jung@users.noreply.github.com>
2025-09-09 11:51:33 +0200

    fix: added CORS Headers + preflight request Options

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight (OPTIONS request)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Force all errors and exceptions to be returned as JSON
 
